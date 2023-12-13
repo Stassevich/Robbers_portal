5149d1fd930b499f5bf00d7b83345236fd037cbe
VikaPupo <vikapupo2004@gmail.com>
2023-12-13 20:37:44 +0300

    add search by name

--- src/Pages/robberspage.jsx
+++ src/Pages/robberspage.jsx
@@ -2,16 +2,48 @@
 
 import Robber from '../Components/robbers/robber';
 
-import { robbersList } from '../helpers/robbersList'
+import { robbersList } from '../helpers/robbersList';
+
+import { useState, useEffect } from 'react';
+
+
+
+const filterList = (searchText, list) => {
+	if (!searchText){
+		return list;
+	}
+	return list.filter(({ name })=>
+	    name.toLowerCase().includes(searchText.toLowerCase())
+	);
+}
+
 
 function RobbersPage() {
+
+	const [List, setList] = useState(robbersList)
+	const [searchQuery, setSearchQuery] = useState('')
+
+	useEffect(() => {
+	    const Debounce = setTimeout(() => {
+	        const filteredList = filterList(searchQuery, robbersList)
+	        setList(filteredList);
+		}, 300);
+	});
+
 	return (
 		<main className="section">
 			<div className="container">
+				
+				<input type="text"
+				        placeholder = "Search..." 
+						value = {searchQuery}
+						onChange={e => setSearchQuery(e.target.value)}
+				/>
+                
 				<h2 className="title-1">Robbers</h2>
 				<ul className="projects">
 
-					{robbersList.map((robber) => {
+					{List.map((robber) => {
 						return < Robber key={robber.id} robbersName={robber.name} img={robber.img} />
 					})}
 
--- src/helpers/robbersList.js
+++ src/helpers/robbersList.js
@@ -4,28 +4,28 @@ import project01big from './../img/projects/02-big.jpg'
 
 const robbersList = [
 	{
-		name: "Name",
+		name: "aaaa",
 		id: 1,
 		description: "Description",
 		img: project01,
 		imgBig: project01big,
 	},
 	{
-		name: "Name",
+		name: "aabb",
 		id: 2,
 		description: "Description",
 		img: project01,
 		imgBig: project01big,
 	},
 	{
-		name: "Name",
+		name: "bbbb",
 		id: 3,
 		description: "Description",
 		img: project01,
 		imgBig: project01big,
 	},
 	{
-		name: "Name",
+		name: "cccc",
 		id: 4,
 		description: "Description",
 		img: project01,
